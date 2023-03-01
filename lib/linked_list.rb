8c5a52ab419609538d4f5706c384eb68718d1d11
Luis <lvas248@gmail.com>
2023-03-01 16:31:41 -0500

    commit tree 0b79c5b1533440207c22f4ed3e9f80027490c563

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -9,6 +9,17 @@ class LinkedList
   end
 
   def reverse!
-    # your code here
+    #switch @tail & @head
+    #cycle through each node and switch prev and next vals
+    curr = @head
+    @head = @tail
+    @tail = curr
+    while !curr.nil?
+      prev = curr.prev_node
+      curr.prev_node = curr.next_node
+      curr.next_node = prev
+      curr = curr.prev_node
+    end
+
   end
 end
